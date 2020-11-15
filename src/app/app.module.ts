6d36fadc69e7c9a59e60c8f33cc4a062164f03fb
Obenembot <embottabi@gmail.com>
2020-11-16 01:22:25 +0200

    count down in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CountdownModule} from 'ngx-countdown'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FormsModule} from '@angular/forms'
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
