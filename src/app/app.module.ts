10b3a9c9e42854b20226c8df11ed3aa9ea719cda
Obenembot <embottabi@gmail.com>
2021-01-02 08:53:16 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,28 @@ import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { RegisterComponent } from './rfs/register.component'
 import { ReactiveFormsModule } from '@angular/forms'
-import { TdfComponent } from './tdf/tdf.component';
-import { UsingFBComponent } from './using-fb/using-fb.component';
-import { VcComponent } from './vc/vc.component';
-import { FaComponent } from './fa/fa.component';
-import { ValidateComponent } from './validate/validate.component';
-import { UsingfirebaseComponent } from './usingfirebase/usingfirebase.component'
-
+import { TdfComponent } from './tdf/tdf.component'
+import { UsingFBComponent } from './using-fb/using-fb.component'
+import { VcComponent } from './vc/vc.component'
+import { FaComponent } from './fa/fa.component'
+import { ValidateComponent } from './validate/validate.component'
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, TdfComponent, UsingFBComponent, VcComponent, FaComponent, ValidateComponent, UsingfirebaseComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    TdfComponent,
+    UsingFBComponent,
+    VcComponent,
+    FaComponent,
+    ValidateComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CountdownModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
